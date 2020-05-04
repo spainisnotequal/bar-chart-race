b9d49a9b2a0ba184741b80d2a3e368e0e975ffb1
spainisnotequal <spainisnotequal@gmail.com>
2020-05-04 10:17:18 +0200

    Define a draw function where we'll add the element to draw

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
-import { select as d3_select } from "https://d3js.org/d3-selection.v1.min.js";
+import * as d3 from "https://unpkg.com/d3?module";
 
+/* --------------------------- */
 /* VARIABLES AND CONFIGURATION */
+/* --------------------------- */
 
+const margin = {
+  top: 80,
+  right: 5,
+  bottom: 5,
+  left: 5,
+};
+const height = 300;
+const width = 300;
+
+/* ----------- */
 /* IMPORT DATA */
+/* ----------- */
 
-/* DRAW THE BAR CHART */
+/* ------------- */
+/* DRAW FUNCTION */
+/* ------------- */
 
+function draw() {
+  // add a svg element
+  const svg = d3
+    .select("#svg-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  // add a red line
+  svg
+    .append("line")
+    .style("stroke", "red")
+    .attr("x1", 50)
+    .attr("y1", 50)
+    .attr("x2", 150)
+    .attr("y2", 150);
+
+  // add a title
+  svg.append("text").attr("class", "title").attr("y", 24).html("Plot title");
+}
+
+// call the draw function and enjoy
+draw();
+
+/* ---------------- */
 /* HELPER FUNCTIONS */
+/* ---------------- */
