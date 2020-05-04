48f222e4cef056a6ebb88e8b6d856b26d8214e00
spainisnotequal <spainisnotequal@gmail.com>
2020-05-04 09:48:57 +0200

    Add a .gitignore file and "npm start" the project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # bar-chart-race
-Examplle of a bar chart race using D3.js
+Example of a bar chart race using D3.js
---
+++ index.js
@@ -0,0 +1,9 @@
+import { select as d3_select } from "https://d3js.org/d3-selection.v1.min.js";
+
+/* VARIABLES AND CONFIGURATION */
+
+/* IMPORT DATA */
+
+/* DRAW THE BAR CHART */
+
+/* HELPER FUNCTIONS */
